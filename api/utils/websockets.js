d2a5d11c6fe5f485a7677f0b2607c14f48f03dd7
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-11 16:51:44 -0500

    added production object to config

--- api/utils/websockets.js
+++ api/utils/websockets.js
@@ -4,15 +4,14 @@ const { Server } = require("socket.io");
 const establishWebSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5000",
+            origin: "http://localhost:3000",
             methods: ["GET", "POST", "PUT", "DELETE"]
         }
     });
 
-    const teamChat = io.of('/teams');
-    const sessionChat = io.of('/sessions');
+    const sessionOrders = io.of('/orders');
 
-    sessionChat.on('connection', (socket) => {
+    sessionOrders.on('connection', (socket) => {
         console.log(`User online - Sessions ${socket.id}`)
 
         socket.on('join_room', (data) => {
@@ -20,16 +19,16 @@ const establishWebSocket = (server) => {
             socket.join(data.room)
         })
 
-        socket.on('new_message', (room) => {
-            sessionChat.to(room).emit("update_feed")
+        socket.on('new_order', (room) => {
+            sessionOrders.to(room).emit("update_feed")
         })
 
-        socket.on('update_message', (room) => {
-            sessionChat.to(room).emit("update_feed")
+        socket.on('update_order', (room) => {
+            sessionOrders.to(room).emit("update_feed")
         })
 
-        socket.on('delete_message', (room) => {
-            sessionChat.to(room).emit("update_feed")
+        socket.on('delete_order', (room) => {
+            sessionOrders.to(room).emit("update_feed")
         })
 
         socket.on('leave_room', (data) => {
@@ -39,31 +38,6 @@ const establishWebSocket = (server) => {
         socket.on('disconnect', (room) => {
             socket.broadcast.to(room).emit('offline')
             console.log(`User offline - Sessions ${socket.id}`)
-        });
-    })
-
-    teamChat.on('connection', (socket) => {
-        console.log(`User online - Teams ${socket.id}`)
-
-        socket.on('join_room', (room) => {
-            socket.join(room)
-        })
-
-        socket.on('new_message', (room) => {
-            teamChat.to(room).emit("update_feed")
-        })
-
-        socket.on('update_message', (room) => {
-            teamChat.to(room).emit("update_feed")
-        })
-
-        socket.on('delete_message', (room) => {
-            teamChat.to(room).emit("update_feed")
-        })
-
-        socket.on('disconnect', (room) => {
-            socket.broadcast.to(room).emit('offline')
-            console.log(`User offline - Teams ${socket.id}`)
         });
     })
 }
