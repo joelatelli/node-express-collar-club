56a2208fe9758eb7f7bdcdf9754f7f67b611b108
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-12 14:26:32 -0500

    Connecting to database with  sequelize

--- bin/www.js
+++ bin/www.js
@@ -23,63 +23,18 @@
 //         console.error(error)
 //     });
 
-// const http = require('http');
-// const Sequelize = require('sequelize');
+const http = require('http');
+const Sequelize = require('sequelize');
 
-// const config = require('../config')[process.env.NODE_ENV || 'development'];
+const config = require('../config')[process.env.NODE_ENV || 'development'];
 
-// const app = require('../app')(config);
+const app = require('../app')(config);
 
-// // Get the configuration for the current environment
-// const sequelizeConfig = config[process.env.NODE_ENV || 'development'];
+// Get the configuration for the current environment
+const sequelizeConfig = config[process.env.NODE_ENV || 'development'];
 
-// // Create a Sequelize instance using the appropriate configuration
-// // const sequelize = new Sequelize(sequelizeConfig);
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// })
-
-// function connectToDatabase() {
-//     sequelize.authenticate().then(() => {
-//         console.log('Database connection success! Sequelize is ready to use...');
-//     }).catch((error) => {
-//         console.error('Unable to connect to the database:', error);
-//         process.exit(1);
-//     });
-
-//     return sequelize;
-// }
-
-// // Connect to the database
-// const db = connectToDatabase();
-
-// // Pass the Sequelize instance to the config for use elsewhere in your application
-// config.db = db;
-
-// const server = http.createServer(app);
-
-// server.listen(process.env.PORT || 3000);
-
-// server.on('listening', () => {
-//     console.log(
-//         `Hi there! I'm listening on port ${server.address().port} in ${app.get('env')} mode.`,
-//     );
-// });
-
-// Import environemnt variables
-require('dotenv').config();
-const { port } = require('../config');
-const { app, http } = require('../app');
-const db = require('../db/models');
-const establishWebSocket = require('../utils/websockets')
+// Create a Sequelize instance using the appropriate configuration
+// const sequelize = new Sequelize(sequelizeConfig);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
@@ -93,16 +48,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 })
 
 function connectToDatabase() {
-    sequelize.authenticate().authenticate()
-        .then(() => {
-            console.log('Database connection success! Sequelize is ready to use...');
-            app.listen(port, () => console.log(`Listening on port ${port}...`)) // Listen for connections
-            http.listen(3030, () => console.log('Web Socket server listening on...'))
-        })
-        .catch(error => {
-            console.log('Database connection failure.');
-            console.error(error)
-        });
+    sequelize.authenticate().then(() => {
+        console.log('Database connection success! Sequelize is ready to use...');
+    }).catch((error) => {
+        console.error('Unable to connect to the database:', error);
+        process.exit(1);
+    });
 
     return sequelize;
 }
