4a437edebdc649df7f35459a3b27791a9c13620c
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-13 16:54:05 -0500

    updating

---
+++ models/words.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, DataTypes) {
+    var Words = sequelize.define(
+      "Words",
+      {
+        // The difficulty cannot be null, and must be a validated
+        difficulty: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            isIn: [["easy", "medium", "hard"]]
+          }
+        },
+        words: {
+          // words cannot be null and must be unique
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isAlpha: true,
+            len: [1, 20]
+          }
+        },
+        // the letter count is suppose to count every character of words entered in the hook
+        letterCount: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        }
+      },
+      {
+        freezeTableName: true //model table name will be the same as the model name
+      }
+    );
+    Words.beforeUpsert(
+      // eslint-disable-next-line no-unused-vars
+      async function (words, options) {
+        let phraseLength = await words.split(" ").filter(c => /\w/.test(c))
+          .length;
+        words.letterCount = phraseLength;
+      },
+      {
+        catch(error) {
+          console.log(error);
+        }
+      }
+    );
+  
+    return Words;
+  };
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const PORT = process.env.PROD_DB_PORT || 8080;
+
+const app = express();
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/HTMLroutes")(app);
+require("./routes/APIroutes")(app);
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
