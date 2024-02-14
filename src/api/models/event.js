5cbff33177473f1fc0282f571104395f0fffe7ba
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-14 16:35:09 -0500

    Updating Event model for migration

--- src/api/models/event.js
+++ src/api/models/event.js
@@ -1,15 +1,15 @@
 const { Model, Sequelize } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Event extends Model {
-    static associate(models) {
-      this.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'user',
-        targetKey: 'id',
-      });
-    }
-  }
+  // class Event extends Model {
+  //   static associate(models) {
+  //     this.belongsTo(models.User, {
+  //       foreignKey: 'userId',
+  //       as: 'user',
+  //       targetKey: 'id',
+  //     });
+  //   }
+  // }
   
   Event.init(
     {
