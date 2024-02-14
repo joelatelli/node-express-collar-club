093e6baec74f26c46c96e52ebf063c5e22ea4b21
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-14 00:30:22 -0500

    Deleting old folders

--- errors/index.js
+++
@@ -1,6 +0,0 @@
-
-const { handleSequelizeErrors } = require('./sequelizeErrors');
-const { handleResourceNotFound } = require('./resourceNotFound');
-const { errorFormatter } = require('./errorFormatter');
-
-module.exports = { handleResourceNotFound, handleSequelizeErrors, errorFormatter }
--- errors/resourceNotFound.js
+++
@@ -1,9 +0,0 @@
-const handleResourceNotFound = (_req, _res, next) => {
-    const error = new Error('The requested resource couldn\'t be found.')
-    error.title = "Resource Not Found";
-    error.errors = { message: 'The request resource couldn\'t be found.' }
-    error.status = 404;
-    next(error)
-}
-
-module.exports = { handleResourceNotFound };
