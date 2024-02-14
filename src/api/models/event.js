a2a37fc0182b23aafa8a055bd9973ef3e1d503a0
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-14 13:00:29 -0500

    Syncing product, event, pet profile models to database

--- src/api/models/event.js
+++ src/api/models/event.js
@@ -3,84 +3,87 @@ const { Model, Sequelize } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = (sequelize, DataTypes) => {
-
-  Event.init({
-    id: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    desc: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lat: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
-        min: -90,
-        max: 90
-    },
-    long: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
-        min: -180,
-        max: 180
-    },
-    private: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    startDate: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isDate: true,
-        validateDate(value) {
-          let enteredDate = new Date(value);
-          let currentDate = new Date();
-          if (enteredDate < currentDate) throw new Error('Start date must be in the future')
-        }
-      }
-    },
-    endDate: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isDate: true,
-        validateDate(value) {
-          let enteredDate = new Date(value);
-          let startDate = new Date(this.startDate);
-          if (enteredDate < startDate) throw new Error('End date is less than start date')
-        }
-      }
-    }
-  }, {
-    setterMethods: {
-      setStartDate: function(value) {
-        let startDate = new Date(value).toISOString().slice(0,19).replace('T', ' ');
-        this.setDataValue('startDate', startDate)
+  Event.init(
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
       },
-      setEndDate: function(value) {
-        let endDate = new Date(value).toISOString().slice(0,19).replace('T', ' ');
-        this.setDataValue('endDate', endDate)
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      desc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lat: {
+          type: DataTypes.DECIMAL,
+          allowNull: true,
+          min: -90,
+          max: 90
+      },
+      long: {
+          type: DataTypes.DECIMAL,
+          allowNull: true,
+          min: -180,
+          max: 180
+      },
+      private: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      startDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          validateDate(value) {
+            let enteredDate = new Date(value);
+            let currentDate = new Date();
+            if (enteredDate < currentDate) throw new Error('Start date must be in the future')
+          }
+        }
+      },
+      endDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          validateDate(value) {
+            let enteredDate = new Date(value);
+            let startDate = new Date(this.startDate);
+            if (enteredDate < startDate) throw new Error('End date is less than start date')
+          }
+        }
       }
-    },
-    sequelize,
-    modelName: 'Event'
-  });
+    }, {
+      setterMethods: {
+        setStartDate: function(value) {
+          let startDate = new Date(value).toISOString().slice(0,19).replace('T', ' ');
+          this.setDataValue('startDate', startDate)
+        },
+        setEndDate: function(value) {
+          let endDate = new Date(value).toISOString().slice(0,19).replace('T', ' ');
+          this.setDataValue('endDate', endDate)
+        }
+      },
+      sequelize,
+      modelName: 'Event',
+      timestamps: true
+    }
+  );
+
   return Event;
 };
