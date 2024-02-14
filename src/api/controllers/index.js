2aab324982a7511cad563a0746053bb3712d52bf
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-14 15:52:03 -0500

    Adding controller path to event, product, profile controllers to index

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,4 +1,7 @@
 module.exports = {
     authController: require('./authController'),
     userController: require('./userController'),
+    productController: require('./productController'),
+    eventController: require('./eventController'),
+    profileController: require('./profileController'),
   };
